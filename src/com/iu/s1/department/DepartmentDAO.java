8d6c8346c9228f501c5bbb8a8e66246111f7b06b
KAKAPO <84508282+kakapo9090@users.noreply.github.com>
2021-08-12 14:27:50 +0900

    Update DepartmentDAO.java

--- src/com/iu/s1/department/DepartmentDAO.java
+++ src/com/iu/s1/department/DepartmentDAO.java
@@ -38,16 +38,17 @@ public class DepartmentDAO {
 			
 			rs = st.executeQuery();
 			dto = new Depart_EmpDTO();
-			dto.setAr(new ArrayList<EmployeeDTO>());
+			dto.setAr(new ArrayList<EmployeeDTO>()); // dto에 setAr메서드 호출, 어레이리스트 담기
 			
 			while(rs.next()) {
 				EmployeeDTO eDto = new EmployeeDTO();
 				eDto.setLast_name(rs.getString("Last_Name"));
 				eDto.setSalary(rs.getInt("SALARY"));
 				eDto.setHire_date(rs.getString("hire_date"));
-				dto.getAr().add(eDto);
-				dto.setDepartment_name(rs.getString("department_name"));
-			}
+				dto.getAr().add(eDto); //dto 어레이리스트 불러와서 eDto에 담아뒀던 last_name, salary, hire_date넣음
+				dto.setDepartment_name(rs.getString("department_name")); //다른테이블의 컬럼은 따로 붙여줘야함
+			}// 1:1관계에선 그 클래스의 참조변수명에 세터게터 넣어서 리턴하면 되지만 1:N관계에선 어레이리스트를 
+			//리턴해줘야 하기 때문에 add메서드 사용함
 			
 			
 		} catch (Exception e) {
@@ -56,7 +57,7 @@ public class DepartmentDAO {
 		}finally {
 			dbConnect.disConnect(rs, st, con);
 		}
-	return dto;
+	return dto; //어레이리스트<DTO제네릭>타입의 변수 dto 리턴
 		
 	}//getJoin 종료
 	
